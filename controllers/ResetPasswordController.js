412974988e3228ab1ff0cb1e4f2b1bf616523c8d
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-04-08 14:08:08 +0100

    implement send token to user email funtionality

--- controllers/ResetPasswordController.js
+++ controllers/ResetPasswordController.js
@@ -1,5 +1,43 @@
 import db from '../models/db';
+import randomstring from 'randomstring';
+import nodemailer from 'nodemailer';
+
 
 class ResetPasswordController {
+  resetPassword(req, res) {
+    const { email } = req.body;
+    const token = randomstring.generate();
+    const url = `http://localhost:3000/auth/reset_password/${token}`;
+    const emailMailer = process.env.MAILER_EMAIL_ID || 'jo.mydiary@gmail.com';
+    const password = process.env.MAILER_PASSWORD || 'jo.mydiary2019';
 
+    const transporter = nodemailer.createTransport({
+      service: process.env.MAILER_SERVICE_PROVIDER || 'gmail',
+      auth: {
+        user: emailMailer,
+        pass: password
+      }
+    });
+
+    const mailOptions = {
+      from: emailMailer,
+      to: `${email}`,
+      subject: 'Password help has arrived!',
+      html: `<h3> Dear</h3 >
+      <p>You requested for a password reset, kindly use this link: ${url} to reset your password</p>.`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        throw error;
+      } else {
+        res.status(200).json({
+          status: 200,
+          data: 'Kindly check your email for further instructions'
+        });
+      }
+    });
+  }
 }
+
+export default new ResetPasswordController();
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import AuthController from '../../controllers/AuthController';
 import AuthValidator from '../../middlewares/validators/AuthValidator';
-import PasswordResetValidator from '../../middlewares/validators/PasswordResetValidator'
+import PasswordResetValidator from '../../middlewares/validators/PasswordResetValidator';
+import ResetPasswordController from '../../controllers/ResetPasswordController';
 
 const router = express.Router();
 
 router.route('/auth/signup/')
   .post(AuthValidator.Signup, AuthController.signUp);
 router.route('/auth/forgot_password')
-  .post(PasswordResetValidator.resetPassword)
+  .post(PasswordResetValidator.resetPassword, ResetPasswordController.resetPassword);
 
 export default router;
