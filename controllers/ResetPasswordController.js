0ef84f8b2ee5dd61f1ce591949fb88b5d162cce0
Jonathan Williams <jonathanwilliams505@gmail.com>
2019-04-08 15:11:39 +0100

    fix reciever name in email

--- controllers/ResetPasswordController.js
+++ controllers/ResetPasswordController.js
@@ -4,38 +4,42 @@ import nodemailer from 'nodemailer';
 
 
 class ResetPasswordController {
-  resetPassword(req, res) {
+  forgotPassword(req, res) {
     const { email } = req.body;
     const token = randomstring.generate();
     const url = `http://localhost:3000/auth/reset_password/${token}`;
     const emailMailer = process.env.MAILER_EMAIL_ID || 'jo.mydiary@gmail.com';
     const password = process.env.MAILER_PASSWORD || 'jo.mydiary2019';
 
-    const transporter = nodemailer.createTransport({
-      service: process.env.MAILER_SERVICE_PROVIDER || 'gmail',
-      auth: {
-        user: emailMailer,
-        pass: password
-      }
-    });
+    db.query(`SELECT firstName FROM users WHERE email = '${email}'`).then(user => {
+      const receiver = user.rows[0].firstname;
 
-    const mailOptions = {
-      from: emailMailer,
-      to: `${email}`,
-      subject: 'Password help has arrived!',
-      html: `<h3> Dear</h3 >
-      <p>You requested for a password reset, kindly use this link: ${url} to reset your password</p>.`
-    };
+      const transporter = nodemailer.createTransport({
+        service: process.env.MAILER_SERVICE_PROVIDER || 'gmail',
+        auth: {
+          user: emailMailer,
+          pass: password
+        }
+      });
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        throw error;
-      } else {
-        res.status(200).json({
-          status: 200,
-          data: 'Kindly check your email for further instructions'
-        });
-      }
+      const mailOptions = {
+        from: emailMailer,
+        to: `${email}`,
+        subject: 'Password help has arrived!',
+        html: `<h3> Dear ${receiver},</h3>
+        <p>You requested for a password reset, kindly use this link: ${url} to reset your password</p>.`
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          throw error;
+        } else {
+          res.status(200).json({
+            status: 200,
+            data: 'Kindly check your email for further instructions'
+          });
+        }
+      });
     });
   }
 }
--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -9,6 +9,6 @@ const router = express.Router();
 router.route('/auth/signup/')
   .post(AuthValidator.Signup, AuthController.signUp);
 router.route('/auth/forgot_password')
-  .post(PasswordResetValidator.resetPassword, ResetPasswordController.resetPassword);
+  .post(PasswordResetValidator.resetPassword, ResetPasswordController.forgotPassword);
 
 export default router;
